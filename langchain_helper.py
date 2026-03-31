212d8bfa50ee76c1378bc80f71a81b1719fd3145
Adarsh <>
2026-03-31 17:54:57 +0530

    used Groq as LLM

--- langchain_helper.py
+++ langchain_helper.py
@@ -4,13 +4,15 @@ from langchain_classic.chains import LLMChain
 from langchain_classic.chains import SequentialChain
 from dotenv import load_dotenv
 from langchain_community.llms import Ollama
+from langchain_groq import ChatGroq
 
 import os
 load_dotenv()
-os.environ['OPENAI_API_KEY'] = os.getenv('OPENAI_KEY')
+#os.environ['OPENAI_API_KEY'] = os.getenv('OPENAI_KEY')
 
 #llm = OpenAI(temperature=0.7)
-llm = Ollama(model="llama3")
+#llm = Ollama(model="llama3")
+llm = ChatGroq(model_name="llama-3.1-8b-instant")
 
 def generate_restaurant_name_and_items(cuisine):
     # Chain 1: Restaurant Name
